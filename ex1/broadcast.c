819ee6aed4ff350bd3a0217a2165e73a6104685f
PP2012 <klemmster@tusci-mail.ivs.tu-berlin.de>
2012-05-03 13:27:55 +0200

    Fix StrLength

--- ex1/broadcast.c
+++ ex1/broadcast.c
@@ -12,20 +12,20 @@ int main (int argc, char**argv){
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 	//printf("rank: %d size: %d \n", rank, size);
-	
+
 	int length;
 	char buf[256];
-	
+
 	if( rank == 0 ){
-		length = strlen(argv[1]);
+		length = strlen(argv[1])+1;
 		MPI_Bcast(argv[1], length, MPI_CHAR, 0,
 MPI_COMM_WORLD);
 
-	
+
 	} else {
 		MPI_Bcast(&buf, 256, MPI_CHAR, 0, MPI_COMM_WORLD);
 		if( rank == size-1) {
-			printf( "i'm node %d receved msg: %s \n",rank, 
+			printf( "i'm node %d receved msg: %s \n",rank,
 buf);
 
 		}
