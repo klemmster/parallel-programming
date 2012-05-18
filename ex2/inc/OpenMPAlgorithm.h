95968146b97d5df249c8dcca78cb7257efa96cef
Richard Klemm <richy@coding-reality.de>
2012-05-18 16:45:16 +0200

    Only slightly slower omp version

--- ex2/inc/OpenMPAlgorithm.h
+++ ex2/inc/OpenMPAlgorithm.h
@@ -2,19 +2,19 @@
 #define OPENMPALGORITHM_H_W8N1AXO6
 
 #include <string>
+#include <vector>
 #include "Testable.h"
 
 class OpenMPAlgorithm: virtual public Testable
 {
 public:
-    OpenMPAlgorithm (const std::string name):
-        Testable(name){};
+    OpenMPAlgorithm (const std::string name);
     virtual ~OpenMPAlgorithm () {};
     virtual void run(const std::string& A, const std::string& B, const size_t k);
     virtual Matches collect();
 
 private:
-    Matches matches;
+    std::vector< Matches > results;
 };
 
 #endif /* end of include guard: OPENMPALGORITHM_H_W8N1AXO6 */
--- ex2/src/OpenMPAlgorithm.cpp
+++ ex2/src/OpenMPAlgorithm.cpp
@@ -1,35 +1,47 @@
 #include "OpenMPAlgorithm.h"
 #include <omp.h>
+#include <stdio.h>
+
+OpenMPAlgorithm::OpenMPAlgorithm(const std::string name):
+    Testable(name)
+{
+    results = std::vector< Matches >(omp_get_max_threads());
+}
 
 void OpenMPAlgorithm::run(const std::string& A, const std::string& B, const size_t k){
-    size_t rowLength = A.size();
-    size_t colLength = B.size();
+    const size_t rowLength = A.size();
+    const size_t colLength = B.size();
 
-	matches.reserve(rowLength + colLength - 1);
+    size_t rowBegin = 0;
+    size_t col, row, matchSize;
+    #pragma omp parallel for default(none) lastprivate(rowBegin) private( matchSize, col, row) shared(A, B)
+	for(rowBegin=0; rowBegin < rowLength; ++rowBegin) {
 
-    //#pragma omp parallel
-	for(size_t rowBegin = 0; rowBegin < rowLength; ++rowBegin) {
-		unsigned int matchSize = 0;
-		size_t col = 0, row = rowBegin;
+		matchSize = 0;
+		col = 0;
+        row = rowBegin;
 		for(; col < colLength && row < rowLength; ++col, ++row) {
 			if(A[row] == B[col]) {
 				++matchSize;
+
 			}
 			else if ( matchSize > 0 ) {
 				Match match(row-1, col-1, matchSize);
-				matches.push_back(match);
-
+                results[omp_get_thread_num()].push_back(match);
 				matchSize = 0;
 			}
 		}
 		//check if last comparison was true (fetch last match)
 		if ( matchSize > 0 ) {
 				Match match(row-1, col-1, matchSize);
-				matches.push_back(match);
+                results[omp_get_thread_num()].push_back(match);
 		}
 	}
 
-	for(size_t colBegin = 1; colBegin < colLength; ++colBegin) {
+    size_t colBegin = 1;
+    #pragma omp parallel for default(none) lastprivate(colBegin) private(matchSize, col, row) shared(A, B)
+	for(colBegin = 1; colBegin < colLength; ++colBegin) {
+
 		unsigned int matchSize = 0;
 		size_t col = colBegin, row = 0;
 		for(; col < colLength && row < rowLength; ++col, ++row) {
@@ -38,19 +50,23 @@ void OpenMPAlgorithm::run(const std::string& A, const std::string& B, const size
 			}
 			else if ( matchSize > 0 ) {
 				Match match(row-1, col-1, matchSize);
-				matches.push_back(match);
-
+                results[omp_get_thread_num()].push_back(match);
 				matchSize = 0;
 			}
 		}
 		//check if last comparison was true (fetch last match)
 		if ( matchSize > 0 ) {
 				Match match(row-1, col-1, matchSize);
-				matches.push_back(match);
+                results[omp_get_thread_num()].push_back(match);
 		}
 	}
 }
 
 Matches OpenMPAlgorithm::collect(){
-    return matches;
+    Matches collected;
+    std::vector< Matches >::iterator it;
+    for(it=results.begin(); it!=results.end(); ++it){
+        collected.insert(collected.end(), (*it).begin(), (*it).end());
+    }
+    return collected;
 }
