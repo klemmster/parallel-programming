22a4383a1f40c0b38f1e072f02422399d7e02b62
Richard Klemm <klemmster@@gmail.com>
2012-05-24 15:08:11 +0200

    Fix real benchmark.sh for server

--- ex2/benchmark.sh
+++ ex2/benchmark.sh
@@ -5,13 +5,14 @@ for i in {1..3}
 do
     for THREADS in 1 2 3 4 8
     do
-        for k in {1..100..10}
+        for k in {1..1001..200}
         do
 	    for count in {1..3}
 	    do
-	        echo "dna-A$i dna-B$i; $THREADS; $k;$count;" >> benchmark.txt
+	        echo "$THREADS, $k, $count"
+		echo -n "dna-A1; dna-B1; $THREADS; $k;$count" >> benchmark.txt
+		OMP_NUM_THREADS=$THREADS ./ex2 dna-A$i dna-B$i $k true >> benchmark.txt
 		echo "" >> benchmark.txt
-		OMP_NUM_THREADS=$THREADS ./ex2 dna-A$i dna-B$i $k >> benchmark.txt
             done
 	done
     done
